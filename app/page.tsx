48148883ee5d16afc9246293eefe7956e2bbf75b
snigbar <snigbar@outlook.com>
2023-07-18 15:44:37 +0600

    Add footer component with copyright information

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
+import Footer from "./Components/Footer";
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between px-10 py-8">
     <div className='text-gray-700 text-center'>
      <h1 className='text-3xl font-medium mb-2'>
       Next Todo - Create Todo List
      </h1>
      <p>
-      Create and manage your ToDo tasks
+      Create and manage your ToDos
      </p>
      </div>
+
+
+     {/* footer */}
+     <Footer></Footer>
     </main>
   )
 }
