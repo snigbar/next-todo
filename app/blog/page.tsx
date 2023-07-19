193ed7c7535b526263e64d64ee0bcb32f7341102
snigbar <snigbar@outlook.com>
2023-07-19 20:22:57 +0600

    Add contents/text in blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -6,7 +6,7 @@ const Blog = () => {
   return (
     <div className='px-3 md:px-10 py-8'>
     <Navbar></Navbar>
-    <div className='min-h-[48vh] flex items-center justify-center'>Blog</div>
+    <p className='min-h-[48vh] flex items-center justify-center text-center text-4xl text-violet-700 font-semibold'>'Hardest choices require strongest will'</p>
     <Footer></Footer>
     </div>
   )
